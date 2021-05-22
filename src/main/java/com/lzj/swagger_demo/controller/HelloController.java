65237d51bc10863c26fdf0f252d9f20b31a79b0e
我叫吉姆 <329660626@qq.com>
2021-05-22 15:11:22 +0800

    第一次提交

---
+++ src/main/java/com/lzj/swagger_demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.lzj.swagger_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return "hello";
+    }
+}
